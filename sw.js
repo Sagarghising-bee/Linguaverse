b2524d454922ad1c14c1901d22c9eb45c6d1eeaa
Sagarghising-bee <sagarghising359@gmail.com>
2026-05-20 11:49:47 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Handles PWA install + offline caching
 ═══════════════════════════════ */
 
-const CACHE_NAME = 'Linguaverse-v1';
+const CACHE_NAME = 'Linguaverse-v2';
 
 // All files to cache for offline use
 const CACHE_FILES = [
